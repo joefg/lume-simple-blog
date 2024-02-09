2c42cb11a6c2a34404ad1dd9698990241ed61a3c
joefg <joefullergray@gmail.com>
2024-02-09 19:48:04 +0000

    feature: Use plugins to generate sitemaps and feeds

--- _config.ts
+++ _config.ts
@@ -2,6 +2,8 @@ import lume from "lume/mod.ts";
 import date from "lume/plugins/date.ts";
 import nunjucks from "lume/plugins/nunjucks.ts";
 import postcss from "lume/plugins/postcss.ts";
+import sitemap from "lume/plugins/sitemap.ts";
+import feed from "lume/plugins/feed.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
 import basePath from "lume/plugins/base_path.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
@@ -17,6 +19,8 @@ site
   .copy("favicon.ico")
   .copy("CNAME")
   .use(postcss())
+  .use(sitemap())
+  .use(feed())
   .use(date())
   .use(nunjucks())
   .use(codeHighlight())
